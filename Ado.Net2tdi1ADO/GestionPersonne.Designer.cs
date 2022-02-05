0b028e59872cbdd7ce9ac9357b623da584ef19d7
elhajuojy <souirimehdi311@gmail.com>
2022-02-05 11:01:06 +0100

    Add project files.

---
+++ Ado.Net2tdi1ADO/GestionPersonne.Designer.cs
@@ -0,0 +1,290 @@
+﻿
+namespace Ado.Net2tdi1ADO
+{
+    partial class GestionPersonne
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textCin = new System.Windows.Forms.TextBox();
+            this.TextNom = new System.Windows.Forms.TextBox();
+            this.TextPrenom = new System.Windows.Forms.TextBox();
+            this.TextAge = new System.Windows.Forms.TextBox();
+            this.TextTelephone = new System.Windows.Forms.TextBox();
+            this.btnNouveau = new System.Windows.Forms.Button();
+            this.btnRechercher = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnPerimer = new System.Windows.Forms.Button();
+            this.btnPrecedant = new System.Windows.Forms.Button();
+            this.btnSuivant = new System.Windows.Forms.Button();
+            this.btnDernier = new System.Windows.Forms.Button();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.w = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nom :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Age";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Prenom :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Telephone";
+            // 
+            // textCin
+            // 
+            this.textCin.Location = new System.Drawing.Point(121, 38);
+            this.textCin.Multiline = true;
+            this.textCin.Name = "textCin";
+            this.textCin.Size = new System.Drawing.Size(164, 22);
+            this.textCin.TabIndex = 5;
+            // 
+            // TextNom
+            // 
+            this.TextNom.Location = new System.Drawing.Point(121, 85);
+            this.TextNom.Name = "TextNom";
+            this.TextNom.Size = new System.Drawing.Size(164, 22);
+            this.TextNom.TabIndex = 6;
+            // 
+            // TextPrenom
+            // 
+            this.TextPrenom.Location = new System.Drawing.Point(121, 133);
+            this.TextPrenom.Name = "TextPrenom";
+            this.TextPrenom.Size = new System.Drawing.Size(164, 22);
+            this.TextPrenom.TabIndex = 7;
+            // 
+            // TextAge
+            // 
+            this.TextAge.Location = new System.Drawing.Point(121, 175);
+            this.TextAge.Name = "TextAge";
+            this.TextAge.Size = new System.Drawing.Size(164, 22);
+            this.TextAge.TabIndex = 8;
+            // 
+            // TextTelephone
+            // 
+            this.TextTelephone.Location = new System.Drawing.Point(121, 219);
+            this.TextTelephone.Name = "TextTelephone";
+            this.TextTelephone.Size = new System.Drawing.Size(164, 22);
+            this.TextTelephone.TabIndex = 9;
+            // 
+            // btnNouveau
+            // 
+            this.btnNouveau.Location = new System.Drawing.Point(436, 105);
+            this.btnNouveau.Name = "btnNouveau";
+            this.btnNouveau.Size = new System.Drawing.Size(125, 35);
+            this.btnNouveau.TabIndex = 10;
+            this.btnNouveau.Text = "Nouveau";
+            this.btnNouveau.UseVisualStyleBackColor = true;
+            this.btnNouveau.Click += new System.EventHandler(this.btnNouveau_Click);
+            // 
+            // btnRechercher
+            // 
+            this.btnRechercher.Location = new System.Drawing.Point(567, 85);
+            this.btnRechercher.Name = "btnRechercher";
+            this.btnRechercher.Size = new System.Drawing.Size(125, 35);
+            this.btnRechercher.TabIndex = 11;
+            this.btnRechercher.Text = "Rechercher";
+            this.btnRechercher.UseVisualStyleBackColor = true;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(567, 126);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(125, 35);
+            this.btnSupprimer.TabIndex = 12;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // btnPerimer
+            // 
+            this.btnPerimer.Location = new System.Drawing.Point(38, 259);
+            this.btnPerimer.Name = "btnPerimer";
+            this.btnPerimer.Size = new System.Drawing.Size(125, 35);
+            this.btnPerimer.TabIndex = 13;
+            this.btnPerimer.Text = "Premier";
+            this.btnPerimer.UseVisualStyleBackColor = true;
+            this.btnPerimer.Click += new System.EventHandler(this.btnPerimer_Click);
+            // 
+            // btnPrecedant
+            // 
+            this.btnPrecedant.Location = new System.Drawing.Point(169, 259);
+            this.btnPrecedant.Name = "btnPrecedant";
+            this.btnPrecedant.Size = new System.Drawing.Size(125, 35);
+            this.btnPrecedant.TabIndex = 14;
+            this.btnPrecedant.Text = "Precedant";
+            this.btnPrecedant.UseVisualStyleBackColor = true;
+            this.btnPrecedant.Click += new System.EventHandler(this.btnPrecedant_Click);
+            // 
+            // btnSuivant
+            // 
+            this.btnSuivant.Location = new System.Drawing.Point(436, 259);
+            this.btnSuivant.Name = "btnSuivant";
+            this.btnSuivant.Size = new System.Drawing.Size(125, 35);
+            this.btnSuivant.TabIndex = 15;
+            this.btnSuivant.Text = "Suviant";
+            this.btnSuivant.UseVisualStyleBackColor = true;
+            this.btnSuivant.Click += new System.EventHandler(this.btnSuivant_Click);
+            // 
+            // btnDernier
+            // 
+            this.btnDernier.Location = new System.Drawing.Point(567, 259);
+            this.btnDernier.Name = "btnDernier";
+            this.btnDernier.Size = new System.Drawing.Size(125, 35);
+            this.btnDernier.TabIndex = 16;
+            this.btnDernier.Text = "Dernier";
+            this.btnDernier.UseVisualStyleBackColor = true;
+            this.btnDernier.Click += new System.EventHandler(this.btnDernier_Click);
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Location = new System.Drawing.Point(567, 495);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(125, 35);
+            this.btnQuitter.TabIndex = 17;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(37, 300);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(655, 189);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // w
+            // 
+            this.w.Location = new System.Drawing.Point(300, 259);
+            this.w.Multiline = true;
+            this.w.Name = "w";
+            this.w.Size = new System.Drawing.Size(130, 35);
+            this.w.TabIndex = 19;
+            // 
+            // GestionPersonne
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 579);
+            this.Controls.Add(this.w);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.btnDernier);
+            this.Controls.Add(this.btnSuivant);
+            this.Controls.Add(this.btnPrecedant);
+            this.Controls.Add(this.btnPerimer);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnRechercher);
+            this.Controls.Add(this.btnNouveau);
+            this.Controls.Add(this.TextTelephone);
+            this.Controls.Add(this.TextAge);
+            this.Controls.Add(this.TextPrenom);
+            this.Controls.Add(this.TextNom);
+            this.Controls.Add(this.textCin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "GestionPersonne";
+            this.Text = "GestionPersonne";
+            this.Load += new System.EventHandler(this.GestionPersonne_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textCin;
+        private System.Windows.Forms.TextBox TextNom;
+        private System.Windows.Forms.TextBox TextPrenom;
+        private System.Windows.Forms.TextBox TextAge;
+        private System.Windows.Forms.TextBox TextTelephone;
+        private System.Windows.Forms.Button btnNouveau;
+        private System.Windows.Forms.Button btnRechercher;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnPerimer;
+        private System.Windows.Forms.Button btnPrecedant;
+        private System.Windows.Forms.Button btnSuivant;
+        private System.Windows.Forms.Button btnDernier;
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox w;
+    }
+}
+
---
+++ Ado.Net2tdi1ADO/GestionPersonne.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Ado.Net2tdi1ADO
+{
+    public partial class GestionPersonne : Form
+    {
+        public GestionPersonne()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        SqlConnection ctn = new SqlConnection(@"Data Source=elhajuojy-lapto\mehdi;Initial Catalog=GestionPersonneControleWith9raytiFclick;Integrated Security=True");
+        BindingSource bs = new BindingSource();
+        DataTable table = new DataTable();
+        private void GestionPersonne_Load(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("select * from Personne", ctn);
+           
+            
+
+
+            ctn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            table.Load(dr);
+           
+            bs.DataSource = table;
+            dataGridView1.DataSource = bs;
+
+            ctn.Close();
+           
+        }
+
+        private void btnPerimer_Click(object sender, EventArgs e)
+        {
+            //bs.MoveFirst();
+            afficher(0);
+        }
+
+        private void btnPrecedant_Click(object sender, EventArgs e)
+        {
+            //bs.MovePrevious();
+            int index = RechercheIndex();
+            if (index >0)
+            {
+                afficher(index - 1);
+            }
+            else
+            {
+                MessageBox.Show("Permier est c'est personne");
+            }
+
+            
+        }
+
+        private void btnSuivant_Click(object sender, EventArgs e)
+        {
+            //bs.MoveNext();
+            int index = RechercheIndex();
+            if (index >= table.Rows.Count - 1)
+            {
+                MessageBox.Show("la darnier personne");
+            }
+            else
+            {
+                afficher(index + 1);
+            }
+
+
+
+        }
+
+        private void btnDernier_Click(object sender, EventArgs e)
+        {
+            //bs.MoveLast();
+            afficher(table.Rows.Count-1);
+
+        }
+        
+        public int RechercheIndex()
+        {
+            int pos = -1;
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                if (table.Rows[i][0].ToString() == textCin.Text)
+                {
+                    pos = i;
+                };
+
+                
+
+            };
+            return pos;
+
+        }
+        public void afficher(int pos)
+        {
+            if(pos== -1)
+            {
+                MessageBox.Show("la personne n'existe pas !");
+            }
+            else
+            {
+                textCin.Text = table.Rows[pos][0].ToString();
+                TextNom.Text = table.Rows[pos][1].ToString();
+                TextPrenom.Text = table.Rows[pos][2].ToString();
+                TextAge.Text = table.Rows[pos][4].ToString();
+                TextTelephone.Text = table.Rows[pos][5].ToString();
+            }
+        }
+
+        private void btnNouveau_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
